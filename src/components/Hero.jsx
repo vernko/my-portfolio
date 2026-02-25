1d5b8e8f4231a117c1d6702e8cf70970ebe0205a
Vern Kofford <vernkofford@Verns-MacBook-Pro.local>
2026-02-25 11:38:26 -0700

    feat: style Hero component with CSS
    Resolves #2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
+import './Hero.css'
+
 export default function Hero({ name, title, description, location }) {
   return (
-    <div>
+    <div className="hero">
       <h1>{name}</h1>
       <h2>{title}</h2>
       <p>{description}</p>
