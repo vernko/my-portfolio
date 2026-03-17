574dbaa3d2b16d07ff9ff5d1437f76f4100ea971
Vern Kofford <vernkofford@Verns-MBP.localdomain>
2026-03-17 00:04:02 -0600

    feat: write e2e tests for nav
    Resolves #57

--- e2e/general.spec.ts
+++ e2e/general.spec.ts
@@ -1,6 +1,12 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('General', () => {
+    test('title displays correctly', async ({page}) => {
+        await page.goto('/')
+
+        await expect(page).toHaveTitle('Vern Kofford | Software Engineer in Test')
+    })
+
     test('all sections render correctly', async ({page}) => {
         await page.goto('/')
 
@@ -12,11 +18,5 @@ test.describe('General', () => {
         await expect(page.getByTestId('projects-section')).toBeVisible()
         await expect(page.getByTestId('blog-section')).toBeVisible()
         await expect(page.getByTestId('contact-section')).toBeVisible()
-    })
-
-    test('title displays correctly', async ({page}) => {
-        await page.goto('/')
-
-        await expect(page).toHaveTitle('Vern Kofford | Software Engineer in Test')
     })
 })
